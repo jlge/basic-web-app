182a71c23f5f010ba2f0001365e8f20d1b43821d
Joanna Ge <joannage87@gmail.com>
2023-03-14 16:11:27 -0400

    ad query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,6 +13,11 @@ export default function QueryProcessor(query: string): string {
     let num1 = parseInt(s[s.length - 3]);
     let num2 = parseInt(s[s.length - 1].substring(0, (s[s.length - 1]).length));
     return (num1 + num2).toString();
+  } else if (query.includes("multiplied")) {
+    let s = query.split(" ");
+    let num1 = parseInt(s[s.length - 4]);
+    let num2 = parseInt(s[s.length - 1].substring(0, (s[s.length - 1]).length));
+    return (num1 * num2).toString();
   } else if (query.includes("What is your name?")) {
     return (
       "jk"
@@ -21,8 +26,20 @@ export default function QueryProcessor(query: string): string {
     return ("purple");
   } else if (query.includes("Where are you from?")) {
     return ("chicago");
-  } else if (query.includes("Which of the following numbers is the largest: 51, 87, 68?")) {
-    return ("87");
+  } else if (query.includes("Which of the following numbers is both a square and a cube: ")) {
+    let splitted = query.substring(60).split(", ");
+    let max = -1;
+    for (let i = 0; i < splitted.length; i++) {
+      if (parseInt(splitted[i])) {
+        return ""
+      }
+    }
+    return max.toString();    
+    return ("");
+  } else if (query.includes("")) {
+    return ("");
+  } else if (query.includes("")) {
+    return ("");
   } else if (query.includes("")) {
     return ("");
   } else if (query.includes("")) {
