0c6f1e549b95fe9190faae2164cd1b8e0c7fa77d
Joanna Ge <joannage87@gmail.com>
2023-03-14 15:56:15 -0400

    Ad query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -3,6 +3,18 @@ export default function QueryProcessor(query: string): string {
     return (
       "jk"
     );
+  } else if (query.includes("What is your favorite color?")) {
+    return ("purple");
+  } else if (query.includes("Where are you from?")) {
+    return ("chicago");
+  } else if (query.includes("	Which of the following numbers is the largest: 51, 87, 68?")) {
+    return ("87");
+  } else if (query.includes("")) {
+    return ("");
+  } else if (query.includes("")) {
+    return ("");
+  } else if (query.includes("")) {
+    return ("");
   }
 
   return "";
