b80e2c802781439d27f3ea1b9277c434b77a7d2c
Joanna Ge <joannage87@gmail.com>
2023-03-14 16:17:58 -0400

    Add minus and divide

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -18,27 +18,29 @@ export default function QueryProcessor(query: string): string {
     let num1 = parseInt(s[s.length - 4]);
     let num2 = parseInt(s[s.length - 1].substring(0, (s[s.length - 1]).length));
     return (num1 * num2).toString();
+  } else if (query.includes("divided")) {
+    let s = query.split(" ");
+    let num1 = parseInt(s[s.length - 4]);
+    let num2 = parseInt(s[s.length - 1].substring(0, (s[s.length - 1]).length));
+    return (num1 / num2).toString();
   } else if (query.includes("What is your name?")) {
     return (
       "jk"
     );
-  } else if (query.includes("What is your favorite color?")) {
-    return ("purple");
-  } else if (query.includes("Where are you from?")) {
-    return ("chicago");
   } else if (query.includes("Which of the following numbers is both a square and a cube: ")) {
     let splitted = query.substring(60).split(", ");
-    let max = -1;
     for (let i = 0; i < splitted.length; i++) {
       let num = parseInt(splitted[i]);
       if ((Math.sqrt(num) % 1 === 0) && (Math.cbrt(num) % 1 === 0)) {
         return num.toString();
       }
     }
-    return max.toString();    
     return ("");
-  } else if (query.includes("")) {
-    return ("");
+  } else if (query.includes("minus")) {
+    let s = query.split(" ");
+    let num1 = parseInt(s[s.length - 4]);
+    let num2 = parseInt(s[s.length - 1].substring(0, (s[s.length - 1]).length));
+    return (num1 - num2).toString();
   } else if (query.includes("")) {
     return ("");
   } else if (query.includes("")) {
