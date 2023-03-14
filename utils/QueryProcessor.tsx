c9f83da9ea8bb5366e0144a4ceea29657db4c71c
Joanna Ge <joannage87@gmail.com>
2023-03-14 15:48:06 -0400

    Fix lowercase bug

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,5 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("What is your name?")) {
+  if (query.includes("What is your name?")) {
     return (
       "jk"
     );
